98d11733c5f7ecb76fd6f102193aec4e2f001117
dark-matter08 <chelucien08@gmail.com>
2021-10-18 22:05:14 +0100

    Custom List

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, CardTransition
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.list import OneLineAvatarIconListItem, ThreeLineAvatarIconListItem, ImageLeftWidget
 
 class ScreenManagement(ScreenManager):
     pass
@@ -9,13 +10,20 @@ class MainWindow(MDScreen):
     def refresh_callback(self, *args):
         print("Refreshing...")
 
+# new class
+class CustomListItem(OneLineAvatarIconListItem):
+    def delete_item(self, text):
+        """Delete list item"""
+        self.parent.remove_widget(self)
 
+# modify AddUrlScreen
 class AddUrlScreen(MDScreen):
     def add_url(self, text):
-        """Add the url to list and save the url in shelve file"""
+        """Add the url to list"""
+        self.ids.linklist.add_widget(CustomListItem(text=text)) # new line
         self.ids.linkinput.focus = True
         self.ids.linkinput.text = ''
-        print(text)
+        # removed print line
 
 class MainApp(MDApp):
     def build(self):
