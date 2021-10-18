c739883666869b1f68e80f11b9915cefe1ea0b8e
dark-matter08 <chelucien08@gmail.com>
2021-10-18 22:01:16 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.uix.screen import MDScreen
+
+class ScreenManagement(ScreenManager):
+    pass
+
+class MainWindow(MDScreen):
+    pass
+
+class AddUrlScreen(MDScreen):
+    pass
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = "DeepPurple"
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
